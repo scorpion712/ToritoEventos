886a7b2016c3f99abd36656bee3fd6afa3e950db
scorpion712 <lauutii.127@gmail.com>
2023-10-05 21:28:03 -0300

    Add funcionality to dashboard with calendar

--- src/private/Dashboard.tsx
+++ src/private/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -60,7 +60,7 @@ export default function Dashboard() {
                 <Main open={open}>
                     <DrawerHeader />
                     <Typography variant='h4'>
-                        Eventos
+                            Eventos
                     </Typography>
                     <EventCalendar />
                 </Main>
--- src/public/components/Calendar.tsx
+++ src/public/components/Calendar.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Paper from '@mui/material/Paper'; 
+import Paper from '@mui/material/Paper';
 import { ViewState, EditingState, IntegratedEditing } from '@devexpress/dx-react-scheduler';
 import {
     Scheduler,
@@ -17,6 +17,12 @@ import {
 import { BasicLayout } from './calendar/CalendarEventForm';
 import { AppointmentCard } from './calendar/AppointmentCard';
 import { AppointmentCardHeader } from './calendar/AppointmentCardHeader';
+import { getEvents } from '../services/events/GetEventsService';
+import { EventModel } from '../models/EventModel'; 
+import { deleteEvent } from '../services/events/RemoveEventService';
+import { addEvent } from '../services/events/AddEventService';
+import { ErrorSnackbar } from './ErrorSnackbar';
+
 
 interface AppointmentOwner {
     id: string;
@@ -26,113 +32,18 @@ interface AppointmentOwner {
     email: string;
 }
 
-const appointments = [
-    {
-        title: 'Reunion 100 personas',
-        startDate: new Date(2023, 8, 15, 9, 35),
-        endDate: new Date(2023, 8, 15, 11, 30),
-        id: 0,
-        guests: '100',
-        img: 'https://images.pexels.com/photos/2747449/pexels-photo-2747449.jpeg?cs=srgb&dl=pexels-wolfgang-2747449.jpg&fm=jpg',
-        owners: [
-            {
-                id: Math.random().toString(16).slice(2),
-                name: "Francisco",
-                surname: "Fernandez",
-                phone: "223456789",
-                email: "franfernandez@gmail.com"
-            } as AppointmentOwner
-        ],
-        eventType: "Meet"
-    }, {
-        title: 'Cumpleanos Federico',
-        startDate: new Date(2023, 8, 25, 12, 11),
-        endDate: new Date(2023, 8, 25, 13, 0),
-        id: 1,
-        guests: '20',
-        img: '',
-        owners: [
-            {
-                id: Math.random().toString(16).slice(2),
-                name: "Federico",
-                surname: "Gomez",
-                phone: "2239876543",
-                email: "fedegomez@gmail.com"
-            } as AppointmentOwner
-        ],
-        eventType: "Birthday"
-    }, {
-        title: 'Cumpleanos 34 personas',
-        startDate: new Date(2023, 8, 5, 14, 30),
-        endDate: new Date(2023, 8, 5, 15, 35),
-        id: 2,
-        guests: '34',
-        img: 'https://js.devexpress.com/Demos/DXHotels/Content/Pictures/MeetingRoom-0.jpg',
-        owners: [
-            {
-                id: Math.random().toString(16).slice(2),
-                name: "Francisco",
-                surname: "Fernandez",
-                phone: "223456789",
-                email: "franfernandez@gmail.com"
-            } as AppointmentOwner,
-            {
-                id: Math.random().toString(16).slice(2),
-                name: "Federico",
-                surname: "Gomez",
-                phone: "2239876543",
-                email: "fedegomez@gmail.com"
-            } as AppointmentOwner
-        ],
-        eventType: "Birthday"
-    }, {
-        title: 'Evento Privado Francisco',
-        startDate: new Date(2023, 8, 6, 10, 0),
-        endDate: new Date(2023, 8, 6, 11, 0),
-        id: 3,
-        guests: '30',
-        img: 'https://js.devexpress.com/Demos/DXHotels/Content/Pictures/MeetingRoom-4.jpg',
-        owners: [
-            {
-                id: Math.random().toString(16).slice(2),
-                name: "Francisco",
-                surname: "Fernandez",
-                phone: "223456789",
-                email: "franfernandez@gmail.com"
-            } as AppointmentOwner,
-        ],
-        eventType: "Private"
-    }
-];
-
-const PREFIX = 'Demo';
-
-const classes = {
-    icon: `${PREFIX}-icon`,
-    textCenter: `${PREFIX}-textCenter`,
-    firstRoom: `${PREFIX}-firstRoom`,
-    secondRoom: `${PREFIX}-secondRoom`,
-    thirdRoom: `${PREFIX}-thirdRoom`,
-    header: `${PREFIX}-header`,
-    commandButton: `${PREFIX}-commandButton`,
-};
-
-const getClassByLocation = (location: string) => {
-    if (location === 'Room 1') return classes.firstRoom;
-    if (location === 'Room 2') return classes.secondRoom;
-    return classes.thirdRoom;
-};
-
 const getEventColorByType = (eventType: string) => {
-    switch (eventType) {
-        case "Meet":
+    switch (eventType.toString()) {
+        case "1":
             return "#E9B546";
-        case "Private":
+        case "2":
             return "#AF3B3B";
+        case "3":
+            return "#f1c232";
         default:
             return "#6F1E4F";
     }
-} 
+}
 
 const Appointment = ({
     children, style, ...restProps
@@ -150,44 +61,135 @@ const Appointment = ({
     </Appointments.Appointment>
 );
 
+
 export const EventCalendar = () => {
-    const [data, setData] = React.useState(appointments);
-    const [editingOptions, setEditingOptions] = React.useState({
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const data = await getEvents();
+            setData(data);
+        };
+
+        fetchData();
+    }, [])
+
+
+    const [showSnackBar, setShowSnackBar] = React.useState(false);
+    const [errorMessage, setErrorMessage] = React.useState("");
+    const [data, setData] = React.useState<EventModel[]>([]);
+
+
+    const isFormValid = (data: EventModel) => {
+        if (!data.owners || data.owners?.length <= 0
+            && !data.owners?.some((owner: AppointmentOwner) =>
+                owner.email?.length == 0
+                || owner.name?.length == 0
+                || owner.surname?.length == 0
+                || owner.phone?.length == 0)) {
+            setErrorMessage("Los datos del organizador son obligatorios!");
+            return false;
+        }
+        if (data.startDate.getTime() > data.endDate.getTime()) {
+            setErrorMessage("La fecha de fin del evento no puede ser menor a la de inicio!");
+            return false;
+        }
+        if (data.startDate < new Date()) {
+            setErrorMessage("No puede crear un evento para una fecha anterior!");
+            return false;
+        }
+        if (parseInt(data.guests) < 1) {
+            setErrorMessage("Debe haber al menos un invitado al evento!");
+            return false;
+        }
+
+        // si no tiene foto enviar la de torito
+
+        return true;
+    }
+
+    const isChangeValid = (data: EventModel) => {
+        if (data.owners) {
+            if (data.owners?.length <= 0
+                && !data.owners?.some((owner: AppointmentOwner) =>
+                    owner.email?.length == 0
+                    || owner.name?.length == 0
+                    || owner.surname?.length == 0
+                    || owner.phone?.length == 0)) {
+                setErrorMessage("Los datos del organizador son obligatorios!");
+                return false;
+            }
+        }
+        if (data.startDate) {
+            if (data.startDate.getTime() > data.endDate.getTime()) {
+                setErrorMessage("La fecha de fin del evento no puede ser menor a la de inicio!");
+                return false;
+            }
+            if (data.startDate > new Date()) {
+                setErrorMessage("No puede crear un evento para una fecha anterior!");
+                return false;
+            }
+        }
+        if (data.guests) {
+            if (parseInt(data.guests) < 1) {
+                setErrorMessage("Debe haber al menos un invitado al evento!");
+                return false;
+            }
+        }
+
+        // si no tiene foto enviar la de torito
+
+        return true;
+    }
+
+
+    const handleSnackBarClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setShowSnackBar(false);
+    };
+
+
+    const editingOptions = {
         allowAdding: true,
         allowDeleting: true,
         allowUpdating: true,
         allowDragging: true,
         allowResizing: false,
-    });
+    };
+    const { allowDeleting, allowUpdating, allowDragging } = editingOptions;
+
     const [addedAppointment, setAddedAppointment] = React.useState({});
     const [isAppointmentBeingCreated, setIsAppointmentBeingCreated] = React.useState(false);
 
-    const {
-        allowDeleting, allowUpdating, allowDragging,
-    } = editingOptions;
 
     const onCommitChanges = React.useCallback(({ added, changed, deleted }: ChangeSet) => {
         if (added) {
-            console.log("Agregando: Validar que los campos sean validos - en caso de que no mostar snackbar", added)
-            added.title = `${added.eventType} ${added.guests} invidados`;
+            added.title = `${added.eventType} ${added.guests} invitados`;
 
-            // validar que tenga un organizador con los datos completos
-            // validar que coincidan las fechas (inicio > fin y hora inicio > hora fin)
-            // validar fecha inicio >= hoy
-            // validar que tenga al menos 1 invitado
-            // si no tiene foto, por defecto la de torito
-
-            // si no cumple mostrar un 
-
-            const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-            setData([...data, { id: startingAddedId, ...added }]); 
+            if (isFormValid(added)) {
+                const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+                addEvent(added);
+                setData([...data, { id: startingAddedId, ...added }]);
+            } else {
+                setShowSnackBar(true);
+                return;
+            }
         }
         if (changed) {
-            console.log("Modificando: Validar que los campos sean validos - en caso de que no mostar snackbar", data)
-            setData(data.map(appointment => (
-                changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
+            const appointmentToChange = data.find(appointment => appointment.id === Object.keys(changed)[0]);
+            changed[Object.keys(changed)[0]].title = `${changed[Object.keys(changed)[0]].eventType ? changed[Object.keys(changed)[0]].eventType : appointmentToChange?.eventType} ${changed[Object.keys(changed)[0]].guests ? changed[Object.keys(changed)[0]].guests : appointmentToChange?.guests} invitados`;
+            if (isChangeValid(changed[Object.keys(changed)[0]])) {
+                setData(data.map(appointment => (
+                    changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
+            } else {
+                setShowSnackBar(true);
+                return;
+            }
         }
         if (deleted !== undefined) {
+            deleteEvent(deleted);
             setData(data.filter(appointment => appointment.id !== deleted));
         }
         setIsAppointmentBeingCreated(false);
@@ -251,6 +253,10 @@ export const EventCalendar = () => {
                         updateInterval={10}
                     />
                 </Scheduler>
+
+                <ErrorSnackbar open={showSnackBar}
+                    handleClose={handleSnackBarClose}
+                    errorMessage={errorMessage} />
             </Paper>
         </React.Fragment>
     );
