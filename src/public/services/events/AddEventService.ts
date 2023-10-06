836bdb47b9bc4f47ba845eb25742ca5403addea0
scorpion712 <lauutii.127@gmail.com>
2023-10-05 22:52:09 -0300

    Save event image with event id and save an event with a custom id

--- src/public/services/events/AddEventService.ts
+++ src/public/services/events/AddEventService.ts
@@ -1,41 +1,49 @@
 
-import { Timestamp, addDoc } from "firebase/firestore";
+import { DocumentData, DocumentReference, Timestamp, addDoc, doc, setDoc } from "firebase/firestore";
 import { collection } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 import { db, storage } from "../../../private/services/firebase/Firebase";
 import { EventModel } from "../../models/EventModel";
+import { generateFirestoreId } from "../../utilities/FirebaseIdGenerator";
 
-export const addEvent = async (event: EventModel) => { 
-    const firebaseEvent = {
-        eventType: event.eventType,
-        endDate: Timestamp.fromDate(event.endDate),
-        startDate: Timestamp.fromDate(event.startDate),
-        guests: event.guests,
-        title: event.title,
-        img: await saveImage(event.imgFile),
-        owners: [] as string []
+export const addEvent = async (event: EventModel) => {
+    const eventId = generateFirestoreId();
+    console.log(eventId)
+    try {
+        const firebaseEvent = {
+            eventType: event.eventType,
+            endDate: Timestamp.fromDate(event.endDate),
+            startDate: Timestamp.fromDate(event.startDate),
+            guests: event.guests,
+            title: event.title,
+            img: await saveImage(event.imgFile, eventId),
+            owners: [] as string[]
+        }
+
+        // TO DO: validate if user exists or not
+        
+        event.owners.forEach(async (owner) => {
+            const docRef = await addDoc(collection(db, "users"), owner);
+            firebaseEvent.owners.push(docRef.id);
+        });
+
+        // Add a new document with a generated id.  
+        const eventDocRef = doc(db, "events", eventId); 
+        await setDoc(eventDocRef, firebaseEvent); 
+    } catch (error) {
+        console.log(error)
     }
-
-    // TO DO: validate if user exists or not
-    
-    event.owners.forEach(async (owner) => {
-         const docRef = await addDoc(collection(db, "users"), owner);
-         firebaseEvent.owners.push(docRef.id);
-    });
-
-    // Add a new document with a generated id.
-    await addDoc(collection(db, "events"), firebaseEvent);
 }
 
 
-const saveImage = async (image: File) => {
+const saveImage = async (image: File, eventId: string) => {
     // Define a reference to the Firebase Storage location where you want to upload the file
-    const storageRef = ref(storage, 'some-child');
+    const storageRef = ref(storage, `img_${eventId}`);
     try {
         // Upload the file to Firebase Storage
         const snapshot = await uploadBytes(storageRef, image);
-        
+
         // Get uploaded image location
         return await getDownloadURL(snapshot.ref);
     } catch (error) {
