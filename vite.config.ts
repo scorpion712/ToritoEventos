c88c2591dd8044ad907ec16715fbc3d552c4c215
scorpion712 <lauutii.127@gmail.com>
2023-10-05 22:19:06 -0300

    Use env variables to connect Firebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dotenv from 'dotenv';
 
+// Load environment variables from the .env file
+dotenv.config();
+ 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Configuración para cargar variables de entorno desde archivos .env
+  define: {
+    'import.meta.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+    'import.meta.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+    'import.meta.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+    'import.meta.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+    'import.meta.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+    'import.meta.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+    'import.meta.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID),
+  }
 })
