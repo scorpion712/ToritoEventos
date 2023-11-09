8cd668fcf3d705b6fbb4415820a1ac97aeab05de
scorpion712 <lauutii.127@gmail.com>
2023-11-08 23:08:50 -0300

    fix hamburguer icon

--- src/components/layouts/MenuAppBar.tsx
+++ src/components/layouts/MenuAppBar.tsx
@@ -4,16 +4,19 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';  
+import AccountCircle from '@mui/icons-material/AccountCircle';
 import Menu from '@mui/material/Menu';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import { useSelector } from 'react-redux'; 
 
 import { Logout } from '..';
+import { AppStore } from '../../redux/store';
+import { Roles } from '../../models/roles';
 
 const drawerWidth = 240;
 
-interface MenuAppBarProps { 
-  handleDrawerOpen: () => void;  
+interface MenuAppBarProps {
+  handleDrawerOpen: () => void;
 }
 
 interface AppBarProps extends MuiAppBarProps {
@@ -36,24 +39,26 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-export default function MenuAppBar(props: MenuAppBarProps) { 
+export default function MenuAppBar(props: MenuAppBarProps) {
 
   const { handleDrawerOpen } = props;
- 
+
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
- 
+
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(event.currentTarget);
   };
 
   const handleClose = () => {
     setAnchorEl(null);
-  };  
+  };
+  const userStore = useSelector((store: AppStore) => store.user);
 
   return (
     <AppBar position="fixed"  >
       <Toolbar>
-        <IconButton
+        {userStore.rol === Roles.ADMIN &&
+         <IconButton
           size="large"
           edge="start"
           color="inherit"
@@ -62,40 +67,40 @@ export default function MenuAppBar(props: MenuAppBarProps) {
           sx={{ mr: 2 }}
         >
           <MenuIcon />
-        </IconButton>
+        </IconButton>}
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Torito Eventos
-        </Typography> 
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <Logout />
-            </Menu>
-          </div> 
+        </Typography>
+        <div>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <Logout />
+          </Menu>
+        </div>
       </Toolbar>
-    </AppBar> 
+    </AppBar>
   );
 }
