733359bf70f9f4133f0692530ff00a44eb67d612
scorpion712 <lauutii.127@gmail.com>
2023-11-08 22:41:36 -0300

    Update barrel

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,8 @@
 export { default as Dashboard } from './Dashboard';
+export { default as EmailNotVerified } from './EmailNotVerified';
+export { default as ErrorPage } from './ErrorPage';
 export { default as Events } from './Events';
+export { default as LoginPage } from './LoginPage';
+export { default as RegistrationPage } from './RegistrationPage';
+export { default as SignUpPage } from './SignUpPage';
 export { default as Users } from './Users';
