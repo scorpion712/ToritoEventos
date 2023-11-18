b37a342a2182be4eff5ee535627d111e5312f9fe
scorpion712 <lauutii.127@gmail.com>
2023-11-18 15:14:14 -0300

    fix user registration

--- src/services/user/createUser.service.ts
+++ src/services/user/createUser.service.ts
@@ -1,13 +1,16 @@
-import { addDoc, collection } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import { AppointmentOwner } from "../../models/AppointmentModel";
 import { db } from "../firebase/firebase";
  
 
 export const registerUserForm = async (user: AppointmentOwner) => {
     try { 
-        const docRef = await addDoc(collection(db, "users"), user);
-        return docRef ? true : false;
-    } catch (error) {
+        // Add a new document with a generated id.  
+        const eventDocRef = doc(db, "users", user.id);  
+        await setDoc(eventDocRef, user); 
+        
+        return true;
+      } catch (error) {
       console.log("Manejo errores globales", error)
       return false;
     }
