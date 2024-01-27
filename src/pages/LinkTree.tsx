42b81719113f4b6309f217d39bee2a509970b782
dlautaro <lautaro.diez@sbi-technology.com>
2024-01-27 13:32:30 -0300

    Responsive model

--- src/pages/LinkTree.tsx
+++ src/pages/LinkTree.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, useMediaQuery, useTheme } from '@mui/material';
 import React from 'react'
 import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
@@ -10,7 +10,7 @@ import RestaurantIcon from '@mui/icons-material/Restaurant';
 import { makeStyles } from '@material-ui/core/styles';
 import NightlightIcon from '@mui/icons-material/Nightlight';
 import InstagramIcon from '@mui/icons-material/Instagram';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp'; 
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -114,6 +114,9 @@ const CustomStepIcon = () => (
 );
 
 function LinkTree() {  
+    const theme = useTheme();
+    const isMobileScreen = useMediaQuery(theme.breakpoints.down('md'));
+
     const classes = useStyles();
     const handleClick = (treeButton: TreeButton) => {
         console.log(`Go to ${treeButton.link}`);
@@ -125,26 +128,30 @@ function LinkTree() {
                 {
                     treeButtons.map((treeButton: TreeButton, _idx: number) => (
                         <React.Fragment key={treeButton.key} >
-                            <Grid item xs={1} sx={{ position: "relative", display: 'flex', alignContent: "center", justifyContent: "center" }} >
-                                {/* Línea vertical que pasa por el centro del Grid */}
-                                <div
-                                    style={{
-                                        width: '4px',
-                                        height: treeButtons.indexOf(treeButton) == 0 || treeButtons.indexOf(treeButton) == treeButtons.length - 1 ? "50%" : "100%",
-                                        backgroundColor: '#27272D', // Puedes ajustar el color de la línea,
-                                        position: "absolute",
-                                        bottom: treeButtons.indexOf(treeButton) == treeButtons.length - 1 ? '' : 0,
-                                    }}
-                                >
-                                </div>
-                                <Stepper  orientation="vertical" sx={{ width: "100%", display: 'flex', alignContent: "center", justifyContent: "center", marginLeft: "1.6rem" }}>
-                                    <Step>
-                                        <StepLabel icon={<CustomStepIcon />} >
-                                        </StepLabel>
-                                    </Step>
-                                </Stepper>
-                            </Grid>
-                            <Grid item xs={11} >
+                            {
+                                !isMobileScreen ? 
+                                <Grid item xs={1} sx={{ position: "relative", display: 'flex', alignContent: "center", justifyContent: "center" }} >
+                                    {/* Línea vertical que pasa por el centro del Grid */}
+                                    <div
+                                        style={{
+                                            width: '4px',
+                                            height: treeButtons.indexOf(treeButton) == 0 || treeButtons.indexOf(treeButton) == treeButtons.length - 1 ? "50%" : "100%",
+                                            backgroundColor: '#27272D', // Puedes ajustar el color de la línea,
+                                            position: "absolute",
+                                            bottom: treeButtons.indexOf(treeButton) == treeButtons.length - 1 ? '' : 0,
+                                        }}
+                                    >
+                                    </div>
+                                    <Stepper  orientation="vertical" sx={{ width: "100%", display: 'flex', alignContent: "center", justifyContent: "center", marginLeft: "1.6rem" }}>
+                                        <Step>
+                                            <StepLabel icon={<CustomStepIcon />} >
+                                            </StepLabel>
+                                        </Step>
+                                    </Stepper>
+                                </Grid>
+                                : <></>
+                            }
+                            <Grid item xs={isMobileScreen ? 12 : 11} >
                                 <Paper className={classes.paper} sx={{ 
                                         p: 2.3,
                                         mt: 1,
@@ -154,7 +161,7 @@ function LinkTree() {
                                     }}
                                     onClick={() => handleClick(treeButton)}>
                                     <Grid container>
-                                        <Grid item xs={8} sm={3}>
+                                        <Grid item xs={8} sm={4}>
                                             <Typography variant="h6" className={classes.responsiveText}>{treeButton.text}</Typography>
                                         </Grid>
                                         <Grid item xs={2}>
